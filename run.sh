0eb2c1be92006ae6293ff862ebf0d751c7e826f2
Scarecrow928 <yushuaibin999@gmail.com>
2021-07-29 11:18:43 +0800

    init

--- run.sh
+++ run.sh
@@ -3,10 +3,36 @@
 EMAIL='email@example.com'
 DOMAIN='domain.com'
 
+function error_exit {
+    docker-compose down
+    exit $1
+}
+
 docker-compose up -d
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+    error_exit $exit_code
+fi
+
 docker exec acme.sh --register-account -m $EMAIL
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+    error_exit $exit_code
+fi
+
 docker exec acme.sh --issue --dns dns_cf -d $DOMAIN -d *.$DOMAIN
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+    error_exit $exit_code
+fi
+
 docker exec acme.sh --install-cert \
     -d $DOMAIN -d *.$DOMAIN \
     --key-file /acme.sh/deploy/key.pem \
     --fullchain-file /acme.sh/deploy/cert.pem
+exit_code=$?
+if [ $exit_code -ne 0 ]; then
+    error_exit $exit_code
+fi
+
+echo "All Done."
