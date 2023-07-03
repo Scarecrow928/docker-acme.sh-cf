cf893d2c4bb4ca4e69efa6846890db3ab29b71c7
Scarecrow928 <yushuaibin999@gmail.com>
2023-07-03 15:22:43 +0800

    env file; run.sh -> issue-cert.sh

--- issue-cert.sh
+++ issue-cert.sh
@@ -1,37 +1,23 @@
-#!/bin/bash
-
-CONFIG_FILE='config.env'
-if [ -f "${CONFIG_FILE}" ]; then
-    source "${CONFIG_FILE}"
-else 
-    echo 'config file not found.'
-    exit 1
-fi
+#!/bin/sh
 
 function error_exit {
     docker compose down
     exit $1
 }
 
-docker compose up -d
+acme.sh --set-default-ca --server letsencrypt
 exit_code=$?
 if [ $exit_code -ne 0 ]; then
     error_exit $exit_code
 fi
 
-docker exec acme.sh --set-default-ca --server letsencrypt
+acme.sh --issue --dns dns_cf -d $DOMAIN -d *.$DOMAIN "$@"
 exit_code=$?
 if [ $exit_code -ne 0 ]; then
     error_exit $exit_code
 fi
 
-docker exec acme.sh --issue --dns dns_cf -d $DOMAIN -d *.$DOMAIN "$@"
-exit_code=$?
-if [ $exit_code -ne 0 ]; then
-    error_exit $exit_code
-fi
-
-docker exec acme.sh --install-cert \
+acme.sh --install-cert \
     -d $DOMAIN -d *.$DOMAIN \
     --key-file /acme.sh/deploy/key.pem \
     --fullchain-file /acme.sh/deploy/cert.pem
