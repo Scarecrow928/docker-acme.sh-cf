b3cc42b54866691921d63195fc3bb2ef791a0a41
Scarecrow928 <yushuaibin999@gmail.com>
2024-05-20 14:44:00 +0800

    script

--- issue-cert.sh
+++ issue-cert.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-acme.sh --set-default-ca --server letsencrypt || exit $?
+set -ex
 
-acme.sh --issue --dns dns_cf -d "$DOMAIN" -d "*.$DOMAIN" "$@" || exit $?
+acme.sh --set-default-ca --server letsencrypt
+
+acme.sh --issue --dns dns_cf -d "$DOMAIN" -d "*.$DOMAIN" "$@"
 
 acme.sh --install-cert \
     -d "$DOMAIN" -d "*.$DOMAIN" \
     --key-file /acme.sh/deploy/key.pem \
-    --fullchain-file /acme.sh/deploy/cert.pem \
-    || exit $?
+    --fullchain-file /acme.sh/deploy/cert.pem
 
 echo "All Done."
